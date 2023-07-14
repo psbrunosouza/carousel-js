edd2f07794d26515768d0545ad24c5e8c50d3a57
psbrunosouza <psbrunosouza@gmail.com>
2023-07-13 23:09:55 -0300

    feat: caminhando no scroll através do click nas arrows

--- src/script.js
+++ src/script.js
@@ -1,8 +1,19 @@
 const carousel = document.querySelector(".carousel");
 const imgs = document.querySelectorAll("img");
+const arrowIcons = document.querySelectorAll(".arrow");
+
+const firstImage = imgs[0];
+
 let isDragStart = false;
 let prevPageX;
 let prevScrollX;
+let firstImageWidth = firstImage.clientWidth + 14;
+
+arrowIcons.forEach((arrow) => {
+  arrow.addEventListener("click", () => {
+    carousel.scrollLeft += arrow.id === "left" ? -firstImageWidth : firstImageWidth;
+  })
+})
 
 imgs.forEach((img) => {
   img.ondragstart = () => {
@@ -10,20 +21,22 @@ imgs.forEach((img) => {
   };
 })
 
+// Função para realizar o drag do element
 carousel.addEventListener("mousedown", (event) => {
   isDragStart = true;
   prevPageX = event.pageX;
   prevScrollX = carousel.scrollLeft;
 });
 
+// Função para realizar o movimento do carousel 
 carousel.addEventListener("mousemove", (event) => {
   if (!isDragStart) return;
-
   let direction = event.pageX - prevPageX;
   carousel.scrollLeft = prevScrollX - direction;
 });
 
-carousel.addEventListener("mouseup", (event) => {
+// Função para desativar o drag do element
+carousel.addEventListener("mouseup", (_event) => {
   isDragStart = false;
 });
 
